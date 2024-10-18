18df65bedfb0b9009b89fd58d0fffe6060984af8
iLoopBreak <teogamer908@gmail.com>
2024-10-18 10:52:19 +0200

    build: Added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/static';
+
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://minimessage-converter.vercel.app",
-  integrations: [react(), tailwind(), sitemap()]
+  site: "https://miniconverter.loopbreak.me",
+  output: "static",
+  integrations: [react(), tailwind(), sitemap(), vercel({
+    webAnalytics: true
+  })]
 });
